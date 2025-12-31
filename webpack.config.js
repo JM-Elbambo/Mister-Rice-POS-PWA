45211484b80ed70e6e1eac69c08e583f16cc1605
JM Elbambo <john.elbambo@gmail.com>
2025-12-31 11:45:38 +0800

    Update build tooling and improve webpack config
    Upgraded several devDependencies including webpack, html-webpack-plugin, and related packages. Refactored webpack.config.js to support environment-based configuration, improved output file naming, and added dev server options. The build now uses different public paths for production and development, and only copies public assets and generates a service worker in production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,58 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 
-module.exports = {
-  entry: "./src/js/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/bundle.[contenthash].js",
-    clean: true,
-    publicPath: "/Mister-Rice-POS-PWA-Build/",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new CopyPlugin({
-      patterns: [{ from: "public", to: "." }],
-    }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    entry: "./src/js/index.js",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "js/[name].[contenthash].js",
+      clean: true,
+      publicPath: isProd ? "/Mister-Rice-POS-PWA/" : "/",
     },
-    port: 3000,
-    hot: true,
-  },
+
+    devtool: isProd ? false : "source-map",
+
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      ...(isProd
+        ? [
+            new CopyWebpackPlugin({
+              patterns: [{ from: "public", to: "" }],
+            }),
+            new WorkboxPlugin.GenerateSW({
+              clientsClaim: true,
+              skipWaiting: true,
+            }),
+          ]
+        : []),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
